34c0a8f9f76c71c9ece18771259439a61718aab4
Zenerzul <70861095+Zenerzul@users.noreply.github.com>
2020-10-08 02:26:05 +0300

    Update asgi.py

--- stepik_tours/asgi.py
+++ stepik_tours/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stepic_tours.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stepik_tours.settings')
 
 application = get_asgi_application()
